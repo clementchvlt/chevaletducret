7fc4e82b70d371ae0e5cc0cc77566c6a8558dfa9
clementchvlt <clement.chevalet@gmail.com>
2018-05-18 11:24:30 +0200

    Capacites

---
+++ src/Capacite.java
@@ -0,0 +1,55 @@
+
+public class Capacite implements ICapacite{
+	private String nom, description;
+	public Capacite (String nom, String descritpion) {
+		setNom(nom);
+		setDescription(description);
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String toString() {
+		
+		return "Capacite[Nom=" + getNom() + ", Description=" + getDescription() + "]";
+		
+	}
+	
+	@Override
+	public void executerEffetDebutTour() throws HearthstoneException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void executerEffetFinTour() throws HearthstoneException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void executerAction(Object cible) throws HearthstoneException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void executerEffetMiseEnJeu(Object cible) throws HearthstoneException {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void executerEffetDisparition(Object cible) throws HearthstoneException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
---
+++ src/Pioche.java
@@ -0,0 +1,9 @@
+
+public class Pioche extends Capacite{
+
+	public Pioche(String nom, String descritpion) {
+		super(nom, descritpion);
+		// TODO Auto-generated constructor stub
+	}
+
+}
