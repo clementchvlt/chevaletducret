63c49c33148e58850dc79a8056e35656e060d511
Clement <Clement@10.10.194.129>
2018-05-22 09:22:37 +0200

    zretyj

--- src/Capacites/Pioche.java
+++ src/Capacites/Pioche.java
@@ -2,4 +2,9 @@ package Capacites;
 
 public class Pioche extends Capacite{
 
+	public Pioche(String nom, String descritpion) {
+		super(nom, descritpion);
+		// TODO Auto-generated constructor stub
+	}
+
 }
--- src/Capacites/Provocation.java
+++ src/Capacites/Provocation.java
@@ -2,4 +2,9 @@ package Capacites;
 
 public class Provocation extends Capacite{
 
+	public Provocation(String nom, String descritpion) {
+		super(nom, descritpion);
+		// TODO Auto-generated constructor stub
+	}
+
 }
