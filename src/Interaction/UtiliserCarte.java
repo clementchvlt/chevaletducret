45b29cadcd54a5c500cbd7a8a66a2e82c8842e0e
Clement <Clement@192.168.1.18>
2018-06-03 22:55:17 +0200

    maj

--- src/Interaction/UtiliserCarte.java
+++ src/Interaction/UtiliserCarte.java
@@ -21,8 +21,11 @@ public class UtiliserCarte extends Interaction{
 	@Override
 	public void Traiter(Object o) {
 		ICarte carte;
+		ICarte carte2;
 		Plateau plateau = null;
 		int cible;
+		String choixCarte2;
+		Object obj;
 		System.out.println("Quelle carte jouer ? (un bout de son nom)");
 		String choixCarte = console.readLine();
 		try {
@@ -34,9 +37,24 @@ public class UtiliserCarte extends Interaction{
 			System.out.println("2. Une autre carte\n");
 			cible = console.readInt();
 			
+			if(cible == 1) {
+				obj=plateau.getAdversaire(plateau.getJoueurCourant()).getHeros();
+				plateau.getJoueurCourant().utiliserCarte(carte, obj);
+			}else if(cible == 2) {
+				System.out.println("Quelle carte tu veux attaquer (donne un boout de son nom\n");
+				choixCarte2= console.readLine();
+				try {
+					carte2=plateau.getAdversaire(plateau.getJoueurCourant()).getCarteEnJeu(choixCarte2);
+					
+					plateau.getJoueurCourant().utiliserCarte(carte, carte2);
+				}catch(HearthstoneException e) {
+					e.printStackTrace();
+				}
+			}
+			
 			
 		}catch(HearthstoneException e) {
-			
+			e.printStackTrace();
 		}
 		
 	}
